109ec2e6996343443eb498ff793ee7033c54d5e1
ChandrakanthReddy93 <chanduveerlas93@gmail.com>
2018-03-13 12:09:35 -0700

    Routing features implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HttpModule, Http } from '@angular/http';
 import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
@@ -10,6 +11,10 @@ import { CalendarComponent } from './components/calendar/calendar.component';
 import { ViewTimeComponent } from './components/view-time/view-time.component';
 import { LabelComponent } from './shared/label/label.component';
 
+const appRoutes: Routes =[
+  {path: 'dashBoard', component: CalendarComponent},
+  {path: 'viewTime',  component: ViewTimeComponent}
+];
 
 export function TranslateStaticLoadFactory(http: Http){
   return new TranslateStaticLoader(http,'./assets/i18n','.json');
@@ -30,6 +35,7 @@ export function TranslateStaticLoadFactory(http: Http){
       useFactory: TranslateStaticLoadFactory,
       deps: [Http]
     }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
