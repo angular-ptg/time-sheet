09e489a2a7ba2598294812e5c3bc6f1a03e58976
saigunichetty <manoj.gunichetty@gmail.com>
2018-03-16 13:47:26 -0700

    implemented Ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ReportTimeComponent } from './report-time/report-time.component';
+import {AgGridModule} from "ag-grid-angular/main";
+import { GridcomponentComponent } from './gridcomponent/gridcomponent.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportTimeComponent
+    ReportTimeComponent,
+    GridcomponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,
+    AgGridModule.withComponents([GridcomponentComponent])
   ],
   providers: [],
   bootstrap: [AppComponent]
